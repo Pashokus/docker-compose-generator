8d99e000809a430f756726e731fd6bfc1e755544
Pashokus <pboiko88@gmail.com>
2017-11-23 18:12:54 +0200

    fix issue with wrapping elements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
+import './static/style.scss';
 
 const store = createStore(reducers);
 
